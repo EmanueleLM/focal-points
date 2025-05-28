2543bb16e9bea26c734160fd6ab1d5653ed99dbd
Emanuele La Malfa <emalfa@CSL15744.local>
2025-05-28 18:36:04 +0100

    Br, Br, Patapim, il mio cappello e' pieno di slim. Nel bosco fitto e misterioso viveva un essere assai curioso.

--- src/utils.py
+++ src/utils.py
@@ -1,23 +1,33 @@
 import itertools
+from collections import Counter
 
 def iterate_schelling_data(data):
     problems = {}
     for d in data:
-        id = d["id"]
-        problems[id] = []
+        idx = d["id"]
+        problems[idx] = []
         text = d["problem"]
-        
-        placeholders = [p for p in d["placeholders"]]
+        placeholders = [d[p] for p in d["placeholders"]]
         if not placeholders:
-            problems[id].append(text)
-            continue 
+            problems[idx].append(text)
+            continue
         
         for elements in itertools.product(*placeholders):
             new_text = text
-            assert len(elements) == len(placeholders), "Mismatch in number of placeholders and elements"
-            for el,p in zip(elements,placeholders):
+            assert len(elements) == len(d["placeholders"]), "Mismatch in number of placeholders and elements"
+            for el,p in zip(elements,d["placeholders"]):
                 new_text = new_text.replace(f"@{p}@", el)
-            problems[id].append(new_text)
+            problems[idx].append(new_text)
             
     return problems
+
+def coordination_index(items:dict):
+    freq = freq_counter = Counter(items)
+    total = sum(freq_counter.values())
+    coordination_index = sum([v*(v-1) for v in dict(freq).values()])/(total*(total-1))
+    
+    return coordination_index, coordination_index * len(freq_counter)
+    
+    
+    
                 
