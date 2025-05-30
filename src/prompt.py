6b4b5e1f344ff714bd95c9c34ca2ba978b2e694f
Emanuele La Malfa <emanuele.lamalfa@mail.polimi.it>
2025-05-30 16:37:30 +0000

    Noooo le weak model nooo

--- src/prompt.py
+++ src/prompt.py
@@ -3,14 +3,14 @@ from dataclasses import dataclass
 @dataclass
 class Level0:
     prefix = ""
-    suffix = "Reply with a single word."
+    suffix = ""
     
 @dataclass
 class Level1:
     prefix = ""
-    suffix = "\nReply with an answer that accounts for the most likely answer the other agent would give. Reply with a single word."
+    suffix = "\nReply with an answer that accounts for the most likely answer the other agent would give."
     
 @dataclass
 class TeamReasoning:
     prefix = ""
-    suffix = "\nPlay like a team. Reply with a single word."
+    suffix = "\nPlay like a team."
