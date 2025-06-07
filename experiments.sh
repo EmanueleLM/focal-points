685444b5d9710b5aa567cf79e63d5ceed9b96772
Emanuele La Malfa <emanuele.lamalfa@mail.polimi.it>
2025-06-07 13:46:23 +0000

    nooo le Ronald MCDonald le hamburger is not coocked NOOOOOOOOOO

--- experiments.sh
+++ experiments.sh
@@ -1,5 +1,20 @@
 #!/bin/bash
 
+# le Schelling
+datasets=("schelling")
+problemtags=("problem-nwp" "problem")
+
+for data in "${datasets[@]}"; do
+  for ptag in "${problemtags[@]}"; do
+    echo "Running main.py with $data and $ptag"
+    python main.py --dataset "$data" --problem-tag "$ptag" --return-sequences 30
+
+    echo "Running metrics.py with $data and $ptag"
+    python metrics.py --dataset "$data" --problem-tag "$ptag"
+  done
+done
+
+# le Amsterdam and Nottingham
 datasets=("amsterdam" "nottingham")
 problemtags=("problem-pick" "problem-guess" "problem-coordinate")
 
