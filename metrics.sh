2c5a582a2331e7dbd4d24377b39570ff62b91d4f
Emanuele La Malfa <emanuele.lamalfa@mail.polimi.it>
2025-09-13 16:46:45 +0000

    New experiments with instruct numeeeeeeeeeeeeee..... RUPPIPIPIPIPIPI STREAMIMIMIMIMI

--- metrics.sh
+++ metrics.sh
@@ -20,30 +20,30 @@ done
 if [[ "$models" == "all" ]]; then
   models=(
   "meta-llama/Llama-3.3-70B-Instruct"
-  "meta-llama/Llama-3.2-1B-Instruct"
-  "meta-llama/Llama-3.2-3B-Instruct"
-  "meta-llama/Llama-3.1-8B-Instruct"
+  # "meta-llama/Llama-3.2-1B-Instruct"
+  # "meta-llama/Llama-3.2-3B-Instruct"
+  # "meta-llama/Llama-3.1-8B-Instruct"
   "meta-llama/Llama-3.1-70B-Instruct"
-  "meta-llama/Meta-Llama-3-8B-Instruct"
+  # "meta-llama/Meta-Llama-3-8B-Instruct"
   "meta-llama/Meta-Llama-3-70B-Instruct"
-  "Qwen/Qwen2.5-7B-Instruct-1M"
-  "Qwen/Qwen2.5-14B-Instruct-1M"
-  "Qwen/Qwen2.5-0.5B-Instruct"
-  "Qwen/Qwen2.5-1.5B-Instruct"
-  "Qwen/Qwen2.5-3B-Instruct"
-  "Qwen/Qwen2.5-7B-Instruct"
-  "Qwen/Qwen2.5-14B-Instruct"
-  "Qwen/Qwen2.5-32B-Instruct"
-  "Qwen/Qwen2.5-72B-Instruct"
-  "Qwen/Qwen2-72B-Instruct"
-  "Qwen/Qwen2-7B-Instruct"
-  "Qwen/Qwen2-1.5B-Instruct"
-  "Qwen/Qwen2-0.5B-Instruct"
-  "google/gemma-3-1b-it"
-  "google/gemma-3-4b-it"
-  "google/gemma-3-12b-it"
-  "google/gemma-3-27b-it"
-  "microsoft/Phi-4-mini-instruct"
+  # "Qwen/Qwen2.5-7B-Instruct-1M"
+  # "Qwen/Qwen2.5-14B-Instruct-1M"
+  # "Qwen/Qwen2.5-0.5B-Instruct"
+  # "Qwen/Qwen2.5-1.5B-Instruct"
+  # "Qwen/Qwen2.5-3B-Instruct"
+  # "Qwen/Qwen2.5-7B-Instruct"
+  # "Qwen/Qwen2.5-14B-Instruct"
+  # "Qwen/Qwen2.5-32B-Instruct"
+  # "Qwen/Qwen2.5-72B-Instruct"
+  # "Qwen/Qwen2-72B-Instruct"
+  # "Qwen/Qwen2-7B-Instruct"
+  # "Qwen/Qwen2-1.5B-Instruct"
+  # "Qwen/Qwen2-0.5B-Instruct"
+  # "google/gemma-3-1b-it"
+  # "google/gemma-3-4b-it"
+  # "google/gemma-3-12b-it"
+  # "google/gemma-3-27b-it"
+  # "microsoft/Phi-4-mini-instruct"
   )
 else
   models=("$models")
@@ -54,55 +54,68 @@ echo "Computing metrics with model: $models"
 for model in "${models[@]}"; do
   echo "Using model: $model"
 
-  # le Schelling
-  datasets=("schelling")
-  problemtags=("problem-nwp" "problem")
-
-  for data in "${datasets[@]}"; do
-    for ptag in "${problemtags[@]}"; do
-
-      echo "Running metrics.py with $data and $ptag"
-      python metrics.py --model "$model" --dataset "$data" --problem-tag "$ptag"
-
-    done
-  done
-
   # le Amsterdam and Nottingham
   datasets=("amsterdam" "nottingham")
   problemtags=("problem-pick" "problem-guess" "problem-coordinate")
 
-  for data in "${datasets[@]}"; do
-    for ptag in "${problemtags[@]}"; do
+  # for data in "${datasets[@]}"; do
+  #   for ptag in "${problemtags[@]}"; do
 
-      echo "Running metrics.py with $data and $ptag"
-      python metrics.py --model "$model" --dataset "$data" --problem-tag "$ptag"
+  #     echo "Running metrics.py with $data and $ptag"
+  #     python metrics.py --model "$model" --dataset "$data" --problem-tag "$ptag"
 
-    done
-  done
+  #   done
+  # done
 
-  # le Amsterdam and Nottingham numeric
-  datasets=("amsterdam_numeric" "nottingham_numeric")
+  # # le Amsterdam and Nottingham numeric
+  # datasets=("amsterdam_numeric" "nottingham_numeric")
+  # problemtags=("problem-pick" "problem-guess" "problem-coordinate")
+
+  # for data in "${datasets[@]}"; do
+  #   for ptag in "${problemtags[@]}"; do
+
+  #     echo "Running metrics.py with $data and $ptag"
+  #     python metrics.py --model "$model" --dataset "$data" --problem-tag "$ptag"
+
+  #   done
+  # done
+
+  # le Amsterdam and Nottingham instruct for saliency
+  datasets=("amsterdam-instruct-saliency" "nottingham-instruct-saliency")
   problemtags=("problem-pick" "problem-guess" "problem-coordinate")
 
   for data in "${datasets[@]}"; do
     for ptag in "${problemtags[@]}"; do
-
       echo "Running metrics.py with $data and $ptag"
       python metrics.py --model "$model" --dataset "$data" --problem-tag "$ptag"
 
     done
   done
 
-  # le Asymmetric Payoff
-  datasets=("asymmetric_payoff")
-  problemtags=("problem-pick" "problem-guess" "problem-coordinate")
+  # # le Asymmetric Payoff
+  # datasets=("asymmetric_payoff")
+  # problemtags=("problem-pick" "problem-guess" "problem-coordinate")
 
-  for data in "${datasets[@]}"; do
-    for ptag in "${problemtags[@]}"; do
+  # for data in "${datasets[@]}"; do
+  #   for ptag in "${problemtags[@]}"; do
 
-      echo "Running metrics.py with $data and $ptag"
-      python metrics.py --model "$model" --dataset "$data" --problem-tag "$ptag"
+  #     echo "Running metrics.py with $data and $ptag"
+  #     python metrics.py --model "$model" --dataset "$data" --problem-tag "$ptag"
 
-    done
-  done
+  #   done
+  # done
+
+  # # le Schelling
+  # datasets=("schelling")
+  # problemtags=("problem-nwp" "problem")
+
+  # for data in "${datasets[@]}"; do
+  #   for ptag in "${problemtags[@]}"; do
+
+  #     echo "Running metrics.py with $data and $ptag"
+  #     python metrics.py --model "$model" --dataset "$data" --problem-tag "$ptag"
+
+  #   done
+  # done
+
 done
