8491629460eb0a803685559b0697d8826a0de9ab
Ido <idoah106@gmail.com>
2025-09-30 20:32:17 +0300

    La vaca Trippa troppa Bombombini Gusini Tralala Lirilira Tung Tung Sahur boneka Tung Tung tralalelo cappuccino assasine Trippi troppa Crocodina fruli frulia

--- experiments.sh
+++ experiments.sh
@@ -31,68 +31,65 @@ done
 # Handle "all-models" option
 if [[ "$models" == "all" ]]; then
   models=(
-  "meta-llama/Llama-3.3-70B-Instruct"
-  "meta-llama/Llama-3.1-70B-Instruct"
-  "meta-llama/Meta-Llama-3-70B-Instruct"
-   "meta-llama/Llama-3.2-1B-Instruct"
-   "meta-llama/Llama-3.2-3B-Instruct"
+   "meta-llama/Llama-3.3-70B-Instruct"
+   "meta-llama/Llama-3.1-70B-Instruct"
+   "meta-llama/Meta-Llama-3-70B-Instruct"
    "meta-llama/Llama-3.1-8B-Instruct"
    "meta-llama/Meta-Llama-3-8B-Instruct"
-   "Qwen/Qwen2.5-7B-Instruct-1M"
-   "Qwen/Qwen2.5-14B-Instruct-1M"
-   "Qwen/Qwen2.5-0.5B-Instruct"
-   "Qwen/Qwen2.5-1.5B-Instruct"
-   "Qwen/Qwen2.5-3B-Instruct"
-   "Qwen/Qwen2.5-7B-Instruct"
-   "Qwen/Qwen2.5-14B-Instruct"
-   "Qwen/Qwen2.5-32B-Instruct"
-   "Qwen/Qwen2.5-72B-Instruct"
+   "meta-llama/Llama-3.2-3B-Instruct"
+   "meta-llama/Llama-3.2-1B-Instruct"
    "Qwen/Qwen2-72B-Instruct"
+   "Qwen/Qwen2.5-72B-Instruct"
+   "Qwen/Qwen2.5-32B-Instruct"
+   "Qwen/Qwen2.5-14B-Instruct-1M"
+   "Qwen/Qwen2.5-14B-Instruct"
+   "Qwen/Qwen2.5-7B-Instruct-1M"
+   "Qwen/Qwen2.5-7B-Instruct"
    "Qwen/Qwen2-7B-Instruct"
+   "Qwen/Qwen2.5-3B-Instruct"
+   "Qwen/Qwen2.5-1.5B-Instruct"
    "Qwen/Qwen2-1.5B-Instruct"
+   "Qwen/Qwen2.5-0.5B-Instruct"
    "Qwen/Qwen2-0.5B-Instruct"
-   "google/gemma-3-1b-it"
-   "google/gemma-3-4b-it"
-   "google/gemma-3-12b-it"
-   "google/gemma-3-27b-it"
-   "microsoft/Phi-4-mini-instruct"
+#   "google/gemma-3-1b-it"
+#   "google/gemma-3-4b-it"
+#   "google/gemma-3-12b-it"
+#   "google/gemma-3-27b-it"
+#   "microsoft/Phi-4-mini-instruct"
   )
 else
   models=("$models")
 fi
 
+datasets=(
+    # amsterdam
+    "amsterdam"
+    "amsterdam-instruct-all-features"
+    "amsterdam-instruct-saliency"
+    # amsterdam_numeric
+    "amsterdam_numeric"
+    "amsterdam_numeric-instruct-all-features"
+    "amsterdam_numeric-instruct-saliency"
+    # asymmetric_payoff
+    "asymmetric_payoff"
+    "asymmetric_payoff-instruct-all-features"
+    "asymmetric_payoff-instruct-saliency"
+    # nottingham
+    "nottingham"
+    "nottingham-instruct-all-features"
+    "nottingham-instruct-saliency"
+    # nottingham_numeric
+    "nottingham_numeric"
+    "nottingham_numeric-instruct-all-features"
+    "nottingham_numeric-instruct-saliency"
+  )
+
+problemtags=("problem-pick" "problem-guess" "problem-coordinate")
+
 echo "Running experiments with model: $models, quantization: $quantization, number of experiments: $num_experiments"
 
- for model in "${models[@]}"; do
-   echo "Using model: $model"
-
-  # le Amsterdam and Nottingham
-  # datasets=("amsterdam" "nottingham")
-  # problemtags=("problem-pick" "problem-guess" "problem-coordinate")
-
-  # for data in "${datasets[@]}"; do
-  #   for ptag in "${problemtags[@]}"; do
-  #     echo "Running main.py with $data and $ptag"
-#       python main.py --model "$model" --dataset "$data" --problem-tag "$ptag" --return-sequences "$num_experiments" --quantization "$quantization" --plot-graphs "$plot_graphs"
-
-  #   done
-  # done
-
-  # # le Amsterdam and Nottingham numeric
-  # datasets=("amsterdam_numeric" "nottingham_numeric")
-  # problemtags=("problem-pick" "problem-guess" "problem-coordinate")
-
-  # for data in "${datasets[@]}"; do
-  #   for ptag in "${problemtags[@]}"; do
-  #     echo "Running main.py with $data and $ptag"
-#       python main.py --model "$model" --dataset "$data" --problem-tag "$ptag"  --return-sequences "$num_experiments" --quantization "$quantization" --plot-graphs "$plot_graphs"
-
-  #   done
-  # done
-
-  # le Amsterdam and Nottingham instruct for saliency
-  datasets=("amsterdam-instruct-all-features" "nottingham-instruct-all-features")
-  problemtags=("problem-pick" "problem-guess" "problem-coordinate")
+for model in "${models[@]}"; do
+  echo "Using model: $model"
 
   for data in "${datasets[@]}"; do
     for ptag in "${problemtags[@]}"; do
@@ -101,29 +98,25 @@ echo "Running experiments with model: $models, quantization: $quantization, numb
 
     done
   done
+done
 
-  # # le Asymmetric Payoff
-  # datasets=("asymmetric_payoff")
-  # problemtags=("problem-pick" "problem-guess" "problem-coordinate")
+datasets=(
+    # schelling
+    "schelling"
+    "schelling-instruct-all-features"
+    "schelling-instruct-saliency"
+  )
 
-  # for data in "${datasets[@]}"; do
-  #   for ptag in "${problemtags[@]}"; do
-  #     echo "Running main.py with $data and $ptag"
-#       python main.py --model "$model" --dataset "$data" --problem-tag "$ptag" --return-sequences "$num_experiments" --quantization "$quantization" --plot-graphs "$plot_graphs"
+problemtags=("problem")
 
-  #   done
-  # done
+for model in "${models[@]}"; do
+  echo "Using model: $model"
 
-  # # le Schelling
-  # datasets=("schelling")
-  # problemtags=("problem-nwp" "problem")
+  for data in "${datasets[@]}"; do
+    for ptag in "${problemtags[@]}"; do
+      echo "Running main.py with $data and $ptag"
+      python main.py --model "$model" --dataset "$data" --problem-tag "$ptag"  --return-sequences "$num_experiments" --quantization "$quantization" --plot-graphs "$plot_graphs"
 
-  # for data in "${datasets[@]}"; do
-  #   for ptag in "${problemtags[@]}"; do
-  #     echo "Running main.py with $data and $ptag"
-#       python main.py --model "$model" --dataset "$data" --problem-tag "$ptag"  --return-sequences "$num_experiments" --quantization "$quantization" --plot-graphs "$plot_graphs"
-
-  #   done
-  # done
-
+    done
+  done
 done
